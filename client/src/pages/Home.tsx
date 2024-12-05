4d971a822e8218d40327f09308ecf666b140447a
Scott Saller <scottmsaller@gmail.com>
2024-12-04 21:18:57 -0500

    Added more seperation of concerns for the client directory

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>this is a test to make sure that react router dom is working</h1>
+            <p>if you're reading this, you owe scott $5.</p>       
+        </div>
+
+    )
+}
+
+export default Home;
