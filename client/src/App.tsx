5f7e4e411d7b05a47b80ffff19f6c1f3e79ec5d8
oglesby80 <OglesbyRodriguez@gmail.com>
2024-12-10 00:17:22 -0500

    1 st push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
+import Footer from './components/Navbar/Footer';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,7 +15,9 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<Signup/>} />
-    </Routes>        
+    </Routes>
+    <Footer/>
+
     </BrowserRouter>
   </StrictMode>
 )
